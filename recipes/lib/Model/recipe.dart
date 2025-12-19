bc85bef3b9bbb578654bc1aa1c16ced4912529d0
PPut789 <poomput.khamhan@gmail.com>
2025-12-19 14:46:18 +0700

    SLIDER

--- recipes/lib/Model/recipe.dart
+++ recipes/lib/Model/recipe.dart
@@ -1,14 +1,65 @@
+import 'package:recipes/Model/ingredient.dart';
+
 class Recipe {
   String imgLabel;
   String imageUrl;
+  String description;
+  List<Ingredient> Ingredients = [];
 
-  Recipe(this.imageUrl, this.imgLabel);
 
-static List<Recipe> samples = [
-  Recipe('assets/images/E479071.webp', 'เสื้อเชิ้ต ผ้าเรยอน'),
-  Recipe('assets/images/E485148.webp', 'ชุดลำลอง Sanrio characters | แขนสั้น'),
-  Recipe('assets/images/Blouson.webp', 'เสื้อ Blouson ทรงสั้น มีซิป | ผ้าเดนิม'),
-  Recipe('assets/images/E481225.webp', 'เสื้อเชิ้ต ผ้าแฟลนเนล | ลายตาราง'),
-  Recipe('assets/images/E480740.webp', 'ยีนส์ ทรงโค้ง (Baggy Curve) | ความยาวพิเศษ - สั้นขึ้น 70 cm*'),
+  Recipe(this.imageUrl, this.imgLabel, this.description,this.Ingredients);
+
+  static List<Recipe> samples = [
+  Recipe('assets/images/Green fish curry.webp', 'เสื้อโปโล ผ้าคอตตอน ลายทาง','เสื้อโปโล ผ้าคอตตอน ลายทาง ผ้าคอตตอน 100% ผ่านกรรมวิธีฟอกเพื่อให้สัมผัสสบายในลุคแคชชวล ลายทางกว้าง',
+  
+  [
+    Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+    Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+    Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+    Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+    Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+    Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+    Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+   ]),
+  Recipe('assets/images/Larb Muang.webp', 'เสื้อโปโลสเวตเตอร์ | แขนสั้น, ลายทาง','เสื้อโปโลสเวตเตอร์ | แขนสั้น, ลายทาง ทำจากผ้าคอตตอนเรยอนสัมผัสเรียบลื่น',
+  [
+    Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+    Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+    Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+    Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+    Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+    Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+    Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+   ]),
+  Recipe('assets/images/Pad Thai.webp', 'เสื้อโปโลสเวตเตอร์ | แขนสั้น','เสื้อโปโลสเวตเตอร์ | แขนสั้น ทำจากผ้าคอตตอนเรยอนสัมผัสเรียบลื่น ซักเครื่องได้',
+  [
+    Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+    Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+    Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+    Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+    Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+    Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+    Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+   ]),
+  Recipe('assets/images/Sweet thai basil smash cocktail.webp', 'เสื้อสเวตเตอร์ โปโล ผ้าถักลายนูน (Ribbed) แบบซักได้','เสื้อสเวตเตอร์ โปโล ผ้าถักลายนูน (Ribbed) แบบซักได้ สัมผัสนุ่มไม่ระคายผิว ดีไซน์ปกสกิปเปอร์ให้ลุคสวยเนี้ยบช่วงคอเมื่อสวมใส่ตัวเดียว',
+  [
+    Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+    Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+    Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+    Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+    Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+    Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+    Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+   ]),
+  Recipe('assets/images/Thai pumpkin curry.webp', 'เสื้อโปโล สเวตเตอร์ ผ้าถักลายนูน (Ribbed) แบบซักได้ | ลายทาง','เสื้อโปโล สเวตเตอร์ ผ้าถักลายนูน (Ribbed) แบบซักได้ ดีไซน์สกิปเปอร์หรูหราเพื่อให้ช่วงคอดูโฉบเฉี่ยว',
+  [
+    Ingredient(name: 'Aubergines', quantity: 2, unit: ''),
+    Ingredient(name: 'Jungle curry paste', quantity: 2, unit: 'tbsp'),
+    Ingredient(name: 'Vegetable stock', quantity: 2, unit: 'cups'),
+    Ingredient(name: 'Kaffir lime leaves', quantity: 3, unit: ''),
+    Ingredient(name: 'Thai chilies', quantity: 2, unit: ''),
+    Ingredient(name: 'Fish sauce', quantity: 1, unit: 'tbsp'),
+    Ingredient(name: 'Thai basil', quantity: 1, unit: 'handful'),
+   ]),
 ];
 }
--- recipes/lib/main.dart
+++ recipes/lib/main.dart
@@ -1,60 +1,82 @@
 import 'package:flutter/material.dart';
 import 'package:recipes/Model/recipe.dart';
-
+import 'package:recipes/recipe-detail.dart';
+ 
 void main() {
   runApp(const RecipeApp());
 }
-
+ 
 class RecipeApp extends StatelessWidget {
   const RecipeApp({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-        ),
-      ),
-      home: const MyHomePage(title: 'Uniqlo Shop'),
+      theme: ThemeData(appBarTheme: const AppBarTheme(centerTitle: true)),
+      home: const MyHomePage(title: 'Recipe Detail'),
     );
   }
 }
-
-
+ 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
-
+ 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
+ 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: SafeArea(child: Container(
-              child: ListView.builder(
-                itemBuilder: (BuildContext context, int index) {
-                  return buildRecipeCard(Recipe.samples[index]);
+      body: SafeArea(
+        child: Container(
+          child: ListView.builder(
+            itemBuilder: (BuildContext context, int index) {
+              return GestureDetector(
+                onTap: () {
+                  print('You Tapped on ${Recipe.samples[index].imgLabel}');
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return RecipeDetail(recipe: Recipe.samples[index]);
+                      },
+                    ),
+                  );
                 },
-                itemCount: Recipe.samples.length
-              ),
-            )),
-        );
+                child: buildRecipeCard(Recipe.samples[index]),
+              );
+            },
+            itemCount: Recipe.samples.length,
+          ),
+        ),
+      ),
+    );
   }
-
-
+ 
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      child: Column(
-        children: <Widget> [
-          Image(image: AssetImage(recipe.imageUrl)),
-          Text(recipe.imgLabel),
-        ],
+      elevation: 2.0,
+      shape: BeveledRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Image(image: AssetImage(recipe.imageUrl)),
+            const SizedBox(height: 14.0),
+            Text(
+              recipe.imgLabel,
+              style: const TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.w500,
+                fontFamily: 'Palatino',
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
