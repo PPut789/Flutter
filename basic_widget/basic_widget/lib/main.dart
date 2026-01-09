632e52a580acaa98c92ad0a1292cdfa3138e21d4
PPut789 <poomput.khamhan@gmail.com>
2026-01-09 14:35:47 +0700

    Widget Tree

--- basic_widget/basic_widget/lib/main.dart
+++ basic_widget/basic_widget/lib/main.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
-
+ 
 void main() {
   runApp(const MainApp());
 }
 
+ 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
-
+ 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
       home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Basic Widgets'),
+          foregroundColor: Colors.white,
+          backgroundColor: const Color.fromARGB(255, 103, 53, 183),
+        ),
         body: Center(
-          child: Text('Hello World!'),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/images/ass1.gif',
+                width: 180,
+              ),
+              const SizedBox(height: 12),
+              const Text(
+                'Hello World!',
+                style: TextStyle(fontSize: 18),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.orange,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: const Text(
+            'Click Me',
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.bold,
+              color: Colors.white
+            ),
+          ),
         ),
       ),
     );
   }
 }
+ 
---
+++ fdsdsfdsf/android/app/src/main/kotlin/com/example/fdsdsfdsf/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.fdsdsfdsf
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
